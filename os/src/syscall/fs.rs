af29035c1156250bd83708b4a31b3c90f987ba62
BowmanChow <bowmanchow@gmail.com>
2021-10-07 14:37:31 +0000

    modify fs.rs

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,9 +1,27 @@
 const FD_STDOUT: usize = 1;
 
 // YOUR JOB: 修改 sys_write 使之通过测试
+const STACK_SIZE: usize = 0x1000;
+
+unsafe fn r_sp() -> usize {
+    let mut sp: usize;
+    asm!("mv {}, sp", out(reg) sp);
+    sp
+}
+
+unsafe fn stack_range() -> (usize, usize) {
+    let sp = r_sp();
+    let top = (sp + STACK_SIZE - 1) & (!(STACK_SIZE - 1));
+    (top - STACK_SIZE, top)
+}
+
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
+            // let (botton, top) = unsafe { stack_range() };
+            // if (buf as usize) < botton || (buf as usize + len) > top {
+            //     return -1;
+            // }
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
             let str = core::str::from_utf8(slice).unwrap();
             print!("{}", str);
